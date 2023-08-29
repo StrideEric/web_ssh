e49bba3e47568f396550744e09ee56b8d9b03aae
Eric L. Stride <strideeric94@gmail.com>
2023-08-28 22:45:05 -0300

    web

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import paramiko
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/execute_ssh_command", methods=["POST"])
+def execute_ssh_command():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    host = "192.168.66.128"
+    username = "mdfk"
+    password = "asdpoasd"
+    command = request.form.get("command")
+    parameters = request.form.get("parameters")
+
+    full_command = f"{command} {parameters}"  # Combina el comando con los parámetros
+
+    try:
+        ssh.connect(host, username=username, password=password)
+        stdin, stdout, stderr = ssh.exec_command(full_command)
+        output = stdout.read().decode("utf-8")
+    except Exception as e:
+        output = str(e)
+    finally:
+        ssh.close()
+
+    return output
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
