89bc14ef42241192d18881a703dac5bc24b22f52
= <alexis.medina301@gmail.com>
2023-08-30 13:31:09 -0300

    le puse el limite xd

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import paramiko
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+db = SQLAlchemy(app)
+
+
+class Contador(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contador = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return str(self.contador) if self.contador is not None else None
+
+
+@app.route("/create_tables", methods=["GET"])
+def create_tables():
+    with app.app_context():
+        db.create_all()
+        __init = Contador(contador=0)
+        db.session.add(__init)
+        db.session.commit()
+        return "Database tables created"
 
 
 @app.route("/")
 def index():
+    contador = Contador.query.all()
+    for c in contador:
+        print(str(c))
+
     return render_template("index.html")
+
 
 @app.route("/execute_ssh_command", methods=["POST"])
 def execute_ssh_command():
@@ -28,14 +54,24 @@ def execute_ssh_command():
 
     if command == "free":
         full_command = f"free {pfree}"  # Combina el comando con los parámetros
-    if command == "lscpu":
+    elif command == "lscpu":
         full_command = f"lscpu {plscpu}"
-    if "meminfo" in command:
+    elif "meminfo" in command:
         full_command = command
-    if command == "vmstat":
+    elif command == "vmstat":
         full_command = command
-    if command == "stress":
-        full_command = "stress --vm 1 --vm-bytes 1G"
+    elif command == "stress":
+        contadores = Contador.query.all()
+        if contadores:
+            primero = contadores[0]
+            if primero.contador >= 5:
+                return "Ya se enviaron muchos comandos stress :<"
+            primero.contador = primero.contador + 1
+            db.session.commit()
+
+        full_command = "stress --vm 1 --vm-bytes 500MB"
+    else:
+        return "Comando inválido"
     full_command = full_command.strip()
 
     # stress --vm 4 --vm-bytes 2G
@@ -48,8 +84,9 @@ def execute_ssh_command():
         output = str(e)
     finally:
         ssh.close()
-    #return render_template("index.html", output=output)
+    # return render_template("index.html", output=output)
     return output
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
